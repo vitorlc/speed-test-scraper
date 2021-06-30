7c17e84f0bade93cbaf0c80ddc02237dce7c5c14
vitorlc <vitor_cirqueira1@hotmail.com>
2021-06-29 21:23:40 -0300

    Feat: add loading and improve ui cli

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 #!/usr/bin/env node
 const puppeteer = require("puppeteer");
+const ora = require('ora');
+const chalk = require('chalk');
+const figlet = require('figlet');
 
 const speedTestSites = [
   {
+    name: 'Minha Conexão',
     url: 'https://www.minhaconexao.com.br',
     waitUntil: ['networkidle0', 'domcontentloaded'],
     waitFor: () => {
@@ -18,6 +22,24 @@ const speedTestSites = [
     }
   },
   {
+    name: 'Speed Test',
+    url: 'https://www.speedtest.net',
+    startButton: '.js-start-test',
+    waitUntil: ['networkidle0', 'domcontentloaded'],
+    waitFor: () => {
+      return !!document.querySelector('.upload-speed').innerText.trim();
+    },
+    getResult: async (page) => {
+      return {
+        ping: await page.$eval('.ping-speed', el => parseFloat(el.innerText)),
+        downloadSpeed: await page.$eval('.download-speed', el => parseFloat(el.innerText)),
+        uploadSpeed: await page.$eval('.upload-speed', el => parseFloat(el.innerText)),
+        server: await page.$eval('.js-data-sponsor', el => el.innerText)
+      }
+    }
+  },
+  {
+    name: 'Fast',
     url: 'https://fast.com',
     waitUntil: 'domcontentloaded',
     waitFor: () => {
@@ -27,43 +49,56 @@ const speedTestSites = [
       await page.click('#show-more-details-link')
       await page.waitForFunction(() => document.querySelector('.succeeded#upload-value'), { timeout: 0, polling: 2000 })
       return {
-        ping: await page.$eval('#latency-value', el => el.innerText),
-        downloadSpeed: await page.$eval('#speed-value', el => el.innerText),
-        uploadSpeed: await page.$eval('#upload-value', el => el.innerText)
-      }
-    }
-  },
-  {
-    url: 'https://www.speedtest.net',
-    startButton: '.js-start-test',
-    waitUntil: ['networkidle0', 'domcontentloaded'],
-    waitFor: () => {
-      return !!document.querySelector('.upload-speed').innerText.trim();
-    },
-    getResult: async (page) => {
-      return {
-        ping: await page.$eval('.ping-speed', el => el.innerText),
-        downloadSpeed: await page.$eval('.download-speed', el => el.innerText),
-        uploadSpeed: await page.$eval('.upload-speed', el => el.innerText)
+        ping: await page.$eval('#latency-value', el => parseFloat(el.innerText)),
+        downloadSpeed: await page.$eval('#speed-value', el => parseFloat(el.innerText)),
+        uploadSpeed: await page.$eval('#upload-value', el => parseFloat(el.innerText))
       }
     }
   }
 ];
 
 const runSpeedScraper = async () => {
+  console.log(chalk.yellow.bold(figlet.textSync('Speed Test\n         Scraper', { horizontalLayout: 'universal smushing', whitespaceBreak: true })));
+  console.log();
+  console.log(chalk.blue("         Author  : ") + chalk.green("Vitor de Lima Cirqueira"));
+  console.log(chalk.blue("         Version : ") + chalk.green("1.1"));
+  console.log(chalk.blue("         Github  : ") + chalk.green("https://github.com/vitorlc"));
+  console.log();
+
+  const spinner = ora();
+  spinner.start(chalk.blue('Iniciando SCRAPER'))
+
   const browser = await puppeteer.launch({ headless: false, args: ['--no-sandbox', '--disable-web-security', '--disable-features=site-per-process'] })
   const page = await browser.newPage()
   await page.setViewport({ width: 1000, height: 1070 })
+  spinner.succeed();
   const result = []
   for (let data of speedTestSites) {
+    console.log()
+    spinner.start(chalk.blue(`Abrindo ${data.name.replace("https://", "")}`)) 
     await page.goto(data.url, { waitUntil: data.waitUntil })
+    spinner.succeed();
     if (data.startButton) await page.click(data.startButton)
+    spinner.start(chalk.blue(`${data.name} - Aguardando Testes`)) 
     await page.waitForFunction(data.waitFor, { timeout: 0, polling: 2000 });
+    spinner.succeed();
     const resultUrl = await data.getResult(page)
-    result.push({ url: data.url, ...resultUrl })
+    spinner.start(chalk.blue(`${data.name} - Obtendo Resultados`)) 
+    result.push({ name: data.name, ...resultUrl })
+    spinner.succeed();
   }
-  console.log(result)
+  spinner.stop()
   await browser.close()
+
+  console.log(chalk.bgBlue.white.bold('\n\n      Resultados:      '))
+  result.forEach((data) => {
+    console.log()
+    console.log(chalk.bgBlue.white.bold(`      ${data.name}      `))
+    console.log(chalk.yellow.bold(`Download: ${data.download}`))
+    console.log(chalk.yellow.bold(`Upload  : ${data.upload}`))
+    console.log(chalk.yellow.bold(`Ping    : ${data.ping}`))
+    console.log(chalk.yellow.bold(`Servidor: ${data.server || '-'}`))
+  })
 }
 
 runSpeedScraper()
